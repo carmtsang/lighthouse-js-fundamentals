d301f5375428e77c493864c3f27080642b63e665
Carmen Tsang <carmtsang89@hotmail.com>
2022-01-30 23:10:41 +0000

    function to calculate age

---
+++ age-calculator.js
@@ -0,0 +1,7 @@
+// ageCalculator function should take 3 parameters - name, yearOfBirth, currentYear
+
+function ageCalculator (name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  
+  return name + " is " + age + " years old.";
+}
