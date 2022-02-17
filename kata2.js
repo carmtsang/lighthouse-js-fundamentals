e1a419873448f4f6c6ab65c4652e3fd36fd1bc60
Carmen Tsang <carmtsang89@hotmail.com>
2022-02-17 05:25:44 +0000

    a function that adds the numbers in an array together when given the condition even or odd

---
+++ kata2.js
@@ -0,0 +1,24 @@
+//only add numbers in the array which match the given condition
+
+const conditionalSum = function(values, condition) {
+  let sum = 0;
+  if (condition === 'even') { 
+    for (let i = 0; i < values.length; i++) { // run through array
+      if (values[i] % 2 === 0) { // check if value is even, if even add together
+       sum += values[i];
+      }
+    }
+  }
+  if (condition === 'odd') {
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] % 2 !== 0) { //if values are odd, add together.
+        sum += values[i];
+      }
+    }
+  }
+  return sum;
+}
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
