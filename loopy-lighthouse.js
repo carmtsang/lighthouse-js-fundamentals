1cc0a5d318989574ef8cc44775662e85a0e8c3b2
Carmen Tsang <carmtsang89@hotmail.com>
2022-03-01 02:58:47 +0000

    turned for loop into a function

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,3 +1,24 @@
+//code for loopy lighthouse 2
+
+function loopyLighthouse(range, multiples, words) {
+  for (let x = range[0]; x <= range[1]; x++) {
+    if ((x % multiples[0] === 0) && (x % multiples[1] === 0)) {
+      console.log(words[0] + words[1]);
+    } else if (x % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (x % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(x);
+    }
+  }
+}
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+
+
+
+/* Code for Loopy Lighthouse 1
 for (let x = 100; x <= 200; x++) { //run from 100 - 200
   if ((x % 3 === 0) && (x % 4 === 0)) {
     console.log("LoopyLighthouse");
@@ -9,3 +30,4 @@ for (let x = 100; x <= 200; x++) { //run from 100 - 200
     console.log(x);
   }
 }
+*/
