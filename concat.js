b3d2faf8e5591fa78bd14027582ca060b4fb2e72
Carmen Tsang <carmtsang89@hotmail.com>
2022-03-01 03:51:21 +0000

    added function to concatenate 2 arrays and sort it from low to high

--- concat.js
+++ concat.js
@@ -9,3 +9,15 @@ console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
 console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
 console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
 console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+
+//concatenate 2 arrays into a single array and sort from low to high
+
+function merge(array1, array2) {
+  const bothArray = array1.concat(array2);
+  const sortArray = bothArray.sort();
+  return sortArray;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
