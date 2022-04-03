73961149951a4835ec01d9860bc24b01ffcad49f
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-03 14:35:38 +0000

    created multiplication table using two for loops

---
+++ kata10.js
@@ -0,0 +1,17 @@
+const multiplicationTable = (maxValue) => {
+  let result = "";
+  for (let i = 1; i <= maxValue; i++) { //loop through number to maxValue
+    for (let j = 1; j <= maxValue; j++) { //loop through number again
+      result += (i * j) + ' '; //multiply the numbers in the two loops, add a space between numbers
+    }
+    result += '\n' //break up the lines in the multiplication table
+  }
+  return result;
+}
+
+
+
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
